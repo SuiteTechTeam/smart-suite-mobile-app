c7f0fd9c5627eddf90f38970ba72a42e7147f9b2
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2025-06-20 20:07:12 -0500

    feat: Enhance IoT device list tile with improved styling and layout refactor: Remove cupertino_icons dependency from pubspec files

--- lib/iot/widgets/iot_device_list_tile.dart
+++ lib/iot/widgets/iot_device_list_tile.dart
@@ -1,17 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../models/iot_device/iot_device.dart';
 
 class IotDeviceListTile extends StatelessWidget {
   final IoTDevice device;
   final VoidCallback? onTap;
+
   const IotDeviceListTile({super.key, required this.device, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(device.name),
-      onTap: onTap,
-      trailing: const Icon(Icons.chevron_right),
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(16),
+        title: Text(
+          device.name,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        trailing: const Icon(
+          Icons.chevron_right,
+          color: Colors.grey,
+        ),
+        onTap: onTap,
+      ),
     );
   }
 }
